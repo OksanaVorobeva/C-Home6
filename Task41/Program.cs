9549b14ab62171bccb2c22502042c86d8fa24164
Oksana <ksanka82@yandex.ru>
2022-12-09 20:17:40 +0300

    Task41

---
+++ Task41/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+        array[i] = new Random().Next(-100, 100);
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    foreach (int el in array) 
+        Console.Write($"{el} ");
+    Console.WriteLine();
+}
+
+int Sum(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >  0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+Console.WriteLine("Введите размерность массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateArray(size);
+PrintArray(array);
+Console.WriteLine(Sum(array));
